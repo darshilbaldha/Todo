0481dd75deac229d331aa529cc24e884df35a909
darshilbaldha <darshilbaldha456@gmail.com>
2024-04-25 00:06:29 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Todo App
+
+A simple web-based todo application that allows users to create and manage their todo notes. Users can add new notes, delete existing ones, and view their todo list.
+
+## Features
+
+- Add new todo notes
+- Delete existing todo notes
+- Responsive design for various screen sizes
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- React
+- NodeJS
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import TodosContainer from "./containers/todos";
+import TodosContextProvider from "./contexts/TodosContext";
+
+function App() {
+  return (
+    <div className="">
+      <div className="pt-20">
+        <h1 className="text-5xl font-extralight text-center">Todo App</h1>
+      </div>
+      <TodosContextProvider>
+        <TodosContainer />
+      </TodosContextProvider>
+      <div className="text-center text-xs">
+        <p>
+          Created with ❤️ by{" "}
+          <a
+            className="text-blue-600"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Students
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
